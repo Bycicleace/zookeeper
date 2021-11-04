16935f4cfaba86f2291fa0ced0ff98ee0e291622
kvammee <elliott.kvamme@gmail.com>
2021-11-03 20:09:52 -0500

    Updated to add an id route

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const { animals } = require("./data/animals");
 const express = require("express");
 
-const PORT = process.env.PORT || 3301;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 function filterByQuery(query, animalsArray) {
@@ -31,12 +31,27 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 }
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
 app.get('/api/animals', (req, res) => {
     let results = animals;
     if (req.query) {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.sendStatus(404);
+    }
+    
 });
 
 app.listen(PORT, () => {
